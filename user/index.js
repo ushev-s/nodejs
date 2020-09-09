e97b02af48a6c6dd7945dbf9596f3dc8488049ef
Sviataslau <52547739+ushev-s@users.noreply.github.com>
2020-09-09 09:39:57 +0300

    Add files via upload

--- user/index.js
+++ user/index.js
@@ -1,16 +1,16 @@
-const phrases = require('./ru');
+const db = require('../db');
+var log = require('../logger')(module);
 
 function User(name) {
   this.name = name;
 }
 
 User.prototype.hello = function (who) {
-  console.log(`${phrases.Hello} ${who.name}`);
+  log(`${db.getPhrase('Hello')}, ${who.name}`);
 };
 
-console.log('user js is required');
-
+module.exports = User;
 //exports.User = User;
-global.User = User;
+//global.User = User;
 
-console.log(module);
+// console.log(module);
